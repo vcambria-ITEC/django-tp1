c5623bc138dfe59f73e69e1a46bd49631b61aa99
= <=>
2026-04-14 20:48:46 -0300

    Agregando modelo y migraciones.

--- practico1/models.py
+++ practico1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Clima(models.Model):
+    nombre_ciudad = models.CharField(max_length=100)
+    fecha_y_hora = models.DateTimeField()
+    temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.nombre_ciudad} - {self.fecha_y_hora}"
--- practico1/views.py
+++ practico1/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Clima
 
 def bienvenida(request):
     return render(request, 'practico1/index.html')
 
 def clima(request):
-    return render(request, 'practico1/clima.html')
+    climas = Clima.objects.all().order_by('-fecha_y_hora')
+    context = {
+        'climas': climas
+    }
+    return render(request, 'practico1/clima.html', context)
